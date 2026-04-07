f188e06cde4c0978a4082e96be5dcf5e30d51b96
Phil Shao <shaoster@gmail.com>
2026-04-07 19:43:52 -0400

    Set a superuser

--- build.sh
+++ build.sh
@@ -30,3 +30,8 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+# Create superuser non-interactively
+if [[ $CREATE_SUPERUSER ]]; then
+  python manage.py createsuperuser --no-input
+fi
